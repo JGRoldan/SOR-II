cca86b5ea4009ba29e7551518b478b1d3041c984
mauroezeflores <mauroezequielflores48@gmail.com>
2025-03-31 17:28:38 -0300

    Completados y comentados structs de boot.c

--- tp-1/read_boot.c
+++ tp-1/read_boot.c
@@ -2,25 +2,40 @@
 #include <stdlib.h>
 
 typedef struct {
-    unsigned char first_byte;
-    unsigned char start_chs[3];
-    unsigned char partition_type;
-    unsigned char end_chs[3];
-    char start_sector[4];
-    char length_sectors[4];
-} __attribute((packed)) PartitionTable;
+    unsigned char first_byte;//Indica si la particion esta activa 0x80 o inactiva 0x00
+    unsigned char start_chs[3];//Direccion chs donde inicia la particion
+    unsigned char partition_type;//Tipo de particion, 0x01 para FAT12
+    unsigned char end_chs[3];//Direccion chs donde termina la particion
+    char start_sector[4];//Direccion inicio de la particion en LBA
+    char length_sectors[4];//Cantidad de sectores totales de la particion
+} __attribute((packed)) PartitionTable;//Esquema de particionado MBR 
 
 typedef struct {
-    unsigned char jmp[3];
-    char oem[8];
-    unsigned short sector_size; // 2 bytes
-	// {...}  COMPLETAR
-    unsigned int volume_id;
-    char volume_label[11];
-    char fs_type[8]; // Type en ascii
-    char boot_code[448];
-    unsigned short boot_sector_signature;
-} __attribute((packed)) Fat12BootSector;
+    unsigned char jmp[3];//bytes 0-2 
+    char oem[8];//bytes 3-10 
+    unsigned short sector_size; //bytes 11-12, bytes por sector
+    //Completamos con datos FAT Boot Sector
+	unsigned char sector_per_cluster; // byte 13
+    unsigned short reserved_sectors; // bytes 14-15
+    unsigned char num_fats; // byte 16, en FAT12 es 2 --> redundancia
+    unsigned short max_root_entries; // bytes 17-18, maximo de archivos en raiz
+    unsigned short total_sectors; // bytes 19-20, total de sectores en la particion
+    unsigned short media_type; // byte 21, tipo de medio, 0xF8 para FAT12
+    unsigned short fat_size; // bytes 22-23, tamaño de cada FAT 
+    unsigned short sectors_per_track; // bytes 24-25, cantidad de sectores por pista
+    unsigned short num_heads; // bytes 26-27, cantidad de cabezales
+    unsigned int hidden_sectors; // bytes 28-31, cantidad de sectores ocultos
+    unsigned int total_sectors_extension; // bytes 32-35, total de sectores en la particion (0 si alcanzaron los bytes 19-20)
+    unsigned char drive_number; // byte 36, numero de unidad logica
+    //byte 37 no usado
+    unsigned char boot_signature; // byte 38, firma 0x38 para validar los siguientes 3 campos
+    //
+    unsigned int volume_id;//bytes 39-42, ID del volumen
+    char volume_label[11];//bytes 43-53, nombre del volumen
+    char fs_type[8]; // bytes 54-61 Type en ascii
+    char boot_code[448];//bytes 62-509
+    unsigned short boot_sector_signature;//bytes 510-511, firma de arranque 0xAA55
+} __attribute((packed)) Fat12BootSector;//Estructura del Boot sector FAT12
 
 int main() {
     FILE * in = fopen("test.img", "rb");
